6cfb053477d563da91464984dc3d78b8c84cb364
Furkan Karagoz <furkan.karagoz@ericsson.com>
2022-07-27 01:37:00 +0300

    Backend stage and running localhost
    Javaspring boot configuration completed.

---
+++ src/main/java/com/example/questappspringproject/entities/User.java
@@ -0,0 +1,20 @@
+package com.example.questappspringproject.entities;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+
+    @Id
+    Long id;
+
+    String userName;
+    String password;
+}
