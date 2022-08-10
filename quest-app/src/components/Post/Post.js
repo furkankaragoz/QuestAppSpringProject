3c911512b168ec7d228773dc32d1f0e658113952
Furkan Karagoz <furkannkaragozz@gmail.com>
2022-08-11 01:27:06 +0300

    React router is integrated.
    A basic navbar has been written.Components seperation implemented.

--- quest-app/src/components/Post/Post.js
+++ quest-app/src/components/Post/Post.js
@@ -1,47 +1,18 @@
-import React, {useState, useEffect} from "react" ;
-import ReactDOM from "react-dom";
+import React from "react";
+import "../Post/Post.scss" ;
 
 
+function Post(props) {
 
+    const{title,text} = props;
 
-function Post() {
-
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [postList, setPostList] = useState([]);
-
-
-    useEffect(() => {
-        fetch("/posts")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true)
-                setPostList(result)
-            },
-            (error) => {
-                setIsLoaded(true)
-                setError(error)
-            }
-        )
-
-    } , [])
-
-    if(error) {
-        return <div> Error when loading.</div>
-    } else if(!isLoaded) {
-        return <div> Loading data...</div>
-    } else {
-        return(
-            <ul>
-                {postList.map(post =>(
-                    <li>
-                        {post.title} {post.text}
-                    </li>
-                ) )}
-            </ul>
-        )
-    }
+    return(
+        <div className="postContainer">
+            {title}
+            {text}
+        </div>
+    )
+   
 
 
 
