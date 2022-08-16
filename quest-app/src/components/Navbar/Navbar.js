3d120ae7e5036eb60df38264087c10de49e76f1d
Furkan Karagoz <furkannkaragozz@gmail.com>
2022-08-16 23:49:39 +0300

    Material-UI template added for Navbar and Posts.
    Added components for posts and navbar.

--- quest-app/src/components/Navbar/Navbar.js
+++ quest-app/src/components/Navbar/Navbar.js
@@ -1,15 +1,47 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 function Navbar () {
 
+    const classes = useStyles();
     let userId = 5;
     return(
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to={{pathname : '/users/' + userId}}>User</Link></li>
-            </ul>
+
+            <AppBar position="static">
+                <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    <Link to="/">Home</Link>
+                </Typography>
+                <Button color="inherit">Login</Button>
+                <Link to={{pathname : '/users/' + userId}}>User</Link>
+                </Toolbar>
+            </AppBar>
+
         </div>
     )
 
--- quest-app/src/components/Post/Post.js
+++ quest-app/src/components/Post/Post.js
@@ -1,15 +1,101 @@
-import React from "react";
+import React , {useState} from "react";
 import "../Post/Post.scss" ;
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }));
 
 function Post(props) {
 
-    const{title,text} = props;
+    const {title,text} = props;
+    const [expanded, setExpanded] = useState(false);
+    const classes = useStyles();
 
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
     return(
         <div className="postContainer">
-            {title}
-            {text}
+             <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+        subheader="September 14, 2016"
+      />   
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+         {text}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        
+        </CardContent>
+      </Collapse>
+    </Card>
         </div>
     )
    
